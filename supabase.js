8bbeeee03804dd6ffd01b05c602e78812f5efd05
ciriloleo-oss <cirilo.leo@gmail.com>
2026-05-22 07:11:12 -0300

    Fix websocket railway

--- supabase.js
+++ supabase.js
@@ -1,16 +1,14 @@
 const { createClient } = require("@supabase/supabase-js");
-
-if (!process.env.SUPABASE_URL) {
-  throw new Error("SUPABASE_URL não configurada");
-}
-
-if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
-  throw new Error("SUPABASE_SERVICE_ROLE_KEY não configurada");
-}
+const WebSocket = require("ws");
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    realtime: {
+      transport: WebSocket,
+    },
+  }
 );
 
 module.exports = { supabase };
